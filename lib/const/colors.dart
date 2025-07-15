4a9e45fb5900dad396ccce5a25167093b018a335
Milanka08 <milankamuthuni5@gmail.com>
2025-07-15 13:35:17 +0530

    ui create

---
+++ lib/const/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color custom_blue = Color(0xFF1E88E5);
+Color backgroundColor = Colors.white;
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todolist_app/const/colors.dart';
 
 class LoginScreen extends statefullWidget {}
 
@@ -11,26 +12,59 @@ class _statefullWidgetState extends State<statefullWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade50,
+      backgroundColor: backgroundColor,
+
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
             children: [
               SizedBox(height: 20),
+              image(),
+              SizedBox(height: 50),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 15),
                 child: Container(
-                  width: double.infinity,
-                  height: 300,
                   decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('images/Logo.jpeg'),
-                      fit: BoxFit.cover,
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: TextField(
+                    style: TextStyle(fontSize: 18, color: Colors.black),
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.email),
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 15,
+                      ),
+                      hintText: 'Email',
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 144, 197, 223),
+                          width: 2,
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding image() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Container(
+        width: double.infinity,
+        height: 300,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/Logo.jpeg'),
+            fit: BoxFit.cover,
           ),
         ),
       ),
